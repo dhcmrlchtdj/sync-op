383499ee3776b41371b72ecee5d7fc2511a0c002
h11 <me@h11.io>
2023-02-03 10:44:24 +0800

    export everything

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-export { type Op, type OpBuilder, type BasicOp } from "./operation.js"
-export { Operation, select, choose, guard } from "./operation.js"
-
+export * from "./operation.js"
 export * from "./extension.js"
-
-export { type readableChannel, type writableChannel } from "./channel.js"
-export { Channel } from "./channel.js"
+export * from "./channel.js"
+export * from "./option.js"
+export * from "./deferred.js"
